4ff60f06724a962747c75e3b2dfc6a56f3dc1ab7
UsamaBashir786 <12345678+UsamaBashir786@users.noreply.github.com>
2025-10-11 11:55:29 +0500

    v-10

--- assets/js/faqs.js
+++ assets/js/faqs.js
@@ -1,17 +1,39 @@
-        const faqItems = document.querySelectorAll('.faq-item');
+// Question active card
 
-        faqItems.forEach(item => {
-            const question = item.querySelector('.faq-question');
-            
-            question.addEventListener('click', () => {
-                const isActive = item.classList.contains('active');
-                
-                faqItems.forEach(otherItem => {
-                    otherItem.classList.remove('active');
-                });
-                
-                if (!isActive) {
-                    item.classList.add('active');
-                }
-            });
-        });
+document.addEventListener("DOMContentLoaded", function () {
+  const questionCards = document.querySelectorAll(".question-card");
+
+  questionCards.forEach((card) => {
+    card.addEventListener("click", function (e) {
+      questionCards.forEach((c) => c.classList.remove("question-active"));
+      this.classList.add("question-active");
+      e.stopPropagation();
+    });
+  });
+  document.addEventListener("click", function (e) {
+    const isClickInsideCard = e.target.closest(".question-card");
+
+    if (!isClickInsideCard) {
+      questionCards.forEach((card) => card.classList.remove("question-active"));
+    }
+  });
+});
+
+// Faq's
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach((item) => {
+  const question = item.querySelector(".faq-question");
+
+  question.addEventListener("click", () => {
+    const isActive = item.classList.contains("active");
+
+    faqItems.forEach((otherItem) => {
+      otherItem.classList.remove("active");
+    });
+
+    if (!isActive) {
+      item.classList.add("active");
+    }
+  });
+});
