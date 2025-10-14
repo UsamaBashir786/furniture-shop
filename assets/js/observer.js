2107b6bfa748fdbab3811949323f39ff7d5f9ee2
UsamaBashir786 <12345678+UsamaBashir786@users.noreply.github.com>
2025-10-14 19:38:02 +0500

    v-18

--- assets/js/observer.js
+++ assets/js/observer.js
@@ -1,1 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const sections = document.querySelectorAll(".product-card");
 
+  const observerOptions = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.5,
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add(".product-card");
+        observer.unobserve(entry.target);
+      }
+    });
+  }, observerOptions);
+
+  sections.forEach((section) => {
+    observer.observe(section);
+  });
+});
